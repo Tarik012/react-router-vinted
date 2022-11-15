c2e9cef55898c4145b713e48e32bcb902479eb44
TARIK-MAC <administrateur@mbpdetarikmac.home>
2022-11-15 20:22:06 +0100

    add stripe payment

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,5 +1,14 @@
 import { useLocation, Navigate } from "react-router-dom";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "../components/CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51HCObyDVswqktOkX6VVcoA7V2sjOJCUB4FBt3EOiAdSz5vWudpWxwcSY8z2feWXBq6lwMgAb5IVZZ1p84ntLq03H00LDVc2RwP"
+);
+
 const Payment = ({ token }) => {
   const location = useLocation();
   const { price, title } = location.state;
@@ -44,9 +53,10 @@ const Payment = ({ token }) => {
             de protection et frais de port inclus).
           </p>
         </div>
-        <div>Mon strip</div>
-        <div className="btn-payment">
-          <button>Pay</button>
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm token={token} title={title} amount={price} />
+          </Elements>
         </div>
       </div>
     </div>
